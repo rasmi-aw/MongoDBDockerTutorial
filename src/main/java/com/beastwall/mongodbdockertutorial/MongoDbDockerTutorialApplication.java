21ad4839dd7ffffc65b4409bc4a69531c3ba7d3d
rasmi <abdelwadoudrasmi@gmail.com>
2023-09-15 12:11:08 +0200

    Initial commit

---
+++ src/main/java/com/beastwall/mongodbdockertutorial/MongoDbDockerTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.beastwall.mongodbdockertutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbDockerTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbDockerTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beastwall/mongodbdockertutorial/MongoDbDockerTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beastwall.mongodbdockertutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbDockerTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
